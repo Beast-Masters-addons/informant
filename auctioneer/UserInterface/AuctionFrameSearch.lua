6b50607a2002dff6da66811db952c40d5ec24db1
Luke1410 <luke1410@norganna.org>
2007-05-09 15:54:39 +0000

    (fixes #1582) Auctions which the user currently bids on, won't be reenabled while bidding is in progress any more.
    If during a bid progress the snapshot was being updated, which is quite often the case as old snapshots are being updated before a bid is being placed, the auction could be reenabled. This has been fixed, now and auctions which are currently in progress of being bidded on / bought out, won#t be reenabled any more. #1582 only descirbed one out of some more scenarios which could lead to this incorrect behavior.

--- auctioneer/UserInterface/AuctionFrameSearch.lua
+++ auctioneer/UserInterface/AuctionFrameSearch.lua
@@ -1267,11 +1267,13 @@ function AuctionFrameSearch_UpdateAuction(frame, snapshotAuction)
 			auction.buyoutPercentLess = percentLess;
 		end
 
-		-- Update the status.
-		if (snapshotAuction.highBidder) then
-			auction.status = AUCTION_STATUS_HIGH_BIDDER;
-		else
-			auction.status = AUCTION_STATUS_NORMAL;
+		-- Update the status, unless the auction is still in bidding progress
+		if not IsBidInProgress(auction.auctionId) then
+			if (snapshotAuction.highBidder) then
+				auction.status = AUCTION_STATUS_HIGH_BIDDER;
+			else
+				auction.status = AUCTION_STATUS_NORMAL;
+			end
 		end
 
 		-- Force a list update.
