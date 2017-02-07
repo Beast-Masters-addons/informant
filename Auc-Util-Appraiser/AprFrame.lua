b72fcdc0317d7f565badaea8c837c248b7e6a514
brykrys <brykrys@norganna.org>
2017-02-07 10:30:31 +0000

    [APPR-352] Tweak Appraiser Refresh scans

--- Auc-Util-Appraiser/AprFrame.lua
+++ Auc-Util-Appraiser/AprFrame.lua
@@ -1338,7 +1338,7 @@ function private.CreateFrames()
 	end
 
 	function frame.RefreshView(background, link)
-		local itemName, itemMinLevel, itemRarity, filterData
+		local itemName, filterData
 		if not link then
 			link = frame.salebox.link
 			if not link then
@@ -1349,17 +1349,15 @@ function private.CreateFrames()
 			end
 		end
 
-		local lType, itemID, _, petQuality = strsplit(":", link)
+		local lType, itemID = strsplit(":", link)
 		itemID = tonumber(itemID)
 		lType = lType:sub(-4)
 		if not itemID then
 			-- do nothing - itemName is not set so we will display an error message and reset at the end of the function
 		elseif lType == "item" then
-			local name, _, quality, _, minlevel, _, _, _, _, _, _, classID, subClassID = GetItemInfo(link)
+			local name, _, _, _, _, _, _, _, _, _, _, classID, subClassID = GetItemInfo(link)
 			if name then
 				itemName = name
-				if minlevel and minlevel > 0 then itemMinLevel = minlevel end
-				if quality and quality > 0 then itemRarity = quality end
 				filterData = AucAdvanced.Scan.QueryFilterFromID(classID, subClassID)
 			end
 		elseif lType == "epet" then -- last 4 letters of "battlepet"
@@ -1367,8 +1365,6 @@ function private.CreateFrames()
 			local petName, _, petType = C_PetJournal.GetPetInfoBySpeciesID(itemID)
 			if petName then
 				itemName = petName
-				petQuality = tonumber(petQuality)
-				if petQuality and petQuality > 0 then itemRarity = petQuality end
 				filterData = AucAdvanced.Scan.QueryFilterFromID(LE_ITEM_CLASS_BATTLEPET, Const.AC_PetType2SubClassID[petType])
 			end
 		-- else do nothing - itemName will not be set
@@ -1385,11 +1381,11 @@ function private.CreateFrames()
 			aucPrint(_TRANS('APPR_Interface_RefreshingView') :format(itemName))--Refreshing view of {{%s}}
 			if background == true then
 				-- Usage: StartPushedScan(name, minLevel, maxLevel, isUsable, qualityIndex, exactMatch, filterData, options)
-				AucAdvanced.Scan.StartPushedScan(itemName, itemMinLevel, itemMinLevel, nil, itemRarity, exact, filterData)
+				AucAdvanced.Scan.StartPushedScan(itemName, nil, nil, nil, nil, exact, filterData)
 			else
 				AucAdvanced.Scan.PushScan()
 				--Usage: StartScan(name, minUseLevel, maxUseLevel, isUsable, qualityIndex, GetAll, exactMatch, filterData, options)
-				AucAdvanced.Scan.StartScan(itemName, itemMinLevel, itemMinLevel, nil, itemRarity, nil, exact, filterData)
+				AucAdvanced.Scan.StartScan(itemName, nil, nil, nil, nil, nil, exact, filterData)
 			end
 		end
 	end
