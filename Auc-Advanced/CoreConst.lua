cb2eb2f93e6b272170246a2405bb9a98543a58ef
brykrys <brykrys@norganna.org>
2018-01-26 12:37:05 +0000

    [ADV-649] <dev> CoreScan: Several changes to the Commit function, notably that Expired auctions are now fully handled in Stage 2.

--- Auc-Advanced/CoreConst.lua
+++ Auc-Advanced/CoreConst.lua
@@ -132,7 +132,6 @@ local lib = {
 	-- Temporary flags (only used during processing - higher values to leave lower ones free for permanent flags)
 	FLAG_DIRTY = 64,
 	FLAG_EXPIRED = 128,
-	FLAG_CORRUPT = 256,
 
 	ALEVEL_OFF = 0,
 	ALEVEL_MIN = 1,
